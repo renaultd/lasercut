ba57c746f5363b078e255dc7db1e364b4627eac2
Julien Allali <allali.julien@gmail.com>
2016-01-13 17:35:52 +0100

    add full box extension

---
+++ lc.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+
+
+def make_plate(width,iwidth,height,iheight,thickness,wsplit,hsplit,bottom,bottomshift,top,topshift,left,leftshift,right,rightshift):
+    wsplit=wsplit * 2+1
+    hsplit=hsplit * 2+1
+
+    if iwidth==False:
+        width-=thickness*2
+    if iheight==False:
+        height-=thickness*2
+
+    wstep=width/wsplit
+    hstep=height/hsplit        
+    points= []
+    x=0
+    y=0
+    direction = 1
+    if bottom in "f":
+        direction=-1
+        y=thickness
+
+    if bottomshift:
+        y+=thickness
+    if leftshift:
+        x+=thickness
+
+    if bottom=='-':            
+        points.append((x,y))
+        x+=wsplit*wstep
+        if leftshift:
+            x-=thickness
+        if rightshift:
+            x-=thickness
+        points.append((x,y))
+    else:
+        for i in range(wsplit):                 
+            points.append((x,y))
+            if i==0 and leftshift:   
+                x=x+wstep-thickness
+            elif i==wsplit-1 and rightshift:
+                x=x+wstep-thickness
+            else:
+                x=x+wstep
+            points.append((x,y))
+            y=y+direction*thickness
+            direction*=-1
+        y=y+direction*thickness
+
+    if right == bottom:
+        direction*=-1
+    if bottom=='-':
+        if right=='f':
+            direction=-1
+        else:
+            direction=1
+    if right == '-':
+        points.append((x,y))
+        y=y-hstep*hsplit
+        if bottomshift:
+            y+=thickness
+        if topshift:
+            y+=thickness
+        points.append((x,y))
+    else:                
+        for i in range(hsplit):
+            points.append((x,y))
+            if i==0 and bottomshift:
+                y=y-hstep+thickness
+            elif i==hsplit-1 and topshift:
+                y=y-hstep+thickness
+            else:
+                y=y-hstep
+            points.append((x,y))
+            x=x+direction*thickness
+            direction*=-1            
+        x=x+direction*thickness
+    if top != right:
+        direction*=-1
+    if right=='-':
+        if top=='m':
+            direction=-1
+        else:
+            direction=1
+    if top == '-':
+        points.append((x,y))
+        x=x-wstep*wsplit
+        if rightshift:
+            x+=thickness
+        if leftshift:
+            x+=thickness
+        points.append((x,y))            
+    else:
+        for i in range(wsplit):
+            points.append((x,y))
+            if i==0 and rightshift:
+                x=x-wstep+thickness
+            elif i==wsplit-1 and leftshift:
+                x=x-wstep+thickness
+            else:
+                x=x-wstep
+            points.append((x,y))
+            y=y+direction*thickness
+            direction*=-1            
+        y=y+direction*thickness
+    if left == top:
+        direction*=-1
+    if top=='-':
+        if left=='m':
+            direction=-1
+        else:
+            direction=1
+    if left == '-':
+        points.append((x,y))
+        y=y+hstep*hsplit
+        if topshift:
+            y-=thickness
+        if bottomshift:
+            y-=thickness
+        points.append((x,y))                
+    else:
+        for i in range(hsplit):
+            points.append((x,y))
+            if i==0 and topshift:
+                y=y+hstep-thickness
+            elif i==hsplit-1 and bottomshift:
+                y=y+hstep-thickness
+            else:
+                y=y+hstep
+            points.append((x,y))
+            x=x+direction*thickness
+            direction*=-1
+
+    return points
+
+
+def translate_points(p,x,y):
+    return [(a+x,b+y) for (a,b) in p]
+
+def points_to_svgd(p):
+    """
+    p: list of 2 tuples (x, y coordinates)
+    """
+    f = p[0]
+    p = p[1:]
+    svgd = 'M%.3f,%.3f' % f
+    for x in p:
+        svgd += 'L%.3f,%.3f' % x
+    return svgd
+
+if __name__ == '__main__':
+    print make_plate(100,True,100,True,3,5,5,'m',False,'m',False,'m',False,'m',False)
---
+++ lc_fullbox.py
@@ -0,0 +1,162 @@
+import sys
+sys.path.append('/usr/share/inkscape/extensions')
+
+import inkex
+from simplestyle import *
+
+import lc
+
+
+class FullBoxEffect(inkex.Effect):    
+    def __init__(self):
+        inkex.Effect.__init__(self)
+        self.OptionParser.add_option('--width', action = 'store',
+          type = 'float', dest = 'width', default = '10',
+          help = 'width')
+        self.OptionParser.add_option('--depth', action = 'store',
+          type = 'float', dest = 'depth', default = '10',
+          help = 'depth')
+        self.OptionParser.add_option('--height', action = 'store',
+          type = 'float', dest = 'height', default = '10',
+          help = 'height')
+        self.OptionParser.add_option('--thickness', action = 'store',
+          type = 'float', dest = 'thickness', default = '3',
+          help = 'thickness')
+        self.OptionParser.add_option('--wsplit', action = 'store',
+          type = 'int', dest = 'wsplit', default = '3',
+          help = 'horizontal splite')
+        self.OptionParser.add_option('--dsplit', action = 'store',
+          type = 'int', dest = 'dsplit', default = '3',
+          help = 'depth splite')
+        self.OptionParser.add_option('--hsplit', action = 'store',
+          type = 'int', dest = 'hsplit', default = '3',
+          help = 'vertical splite')
+        self.OptionParser.add_option('--unit', action = 'store',
+          type = 'string', dest = 'unit', default = 'mm',
+          help = 'unit')        
+        self.OptionParser.add_option('--inner', action = 'store',
+          type = 'inkbool', dest = 'inner', default = 'True',
+          help = 'inner')
+    def effect(self):
+        width=self.unittouu(str(self.options.width)+self.options.unit)
+        height=self.unittouu(str(self.options.height)+self.options.unit)
+        depth=self.unittouu(str(self.options.depth)+self.options.unit)
+        thickness=self.unittouu(str(self.options.thickness)+self.options.unit)
+
+
+        t = 'translate(' + str(self.view_center[0]) + ',' + \
+            str(self.view_center[1]) + ')'
+        g_attribs = {
+            inkex.addNS('label', 'inkscape'): 'Box' + str(width) + "x"+str(height),
+            'transform': t}
+        g = inkex.etree.SubElement(self.current_layer, 'g', g_attribs)
+
+        # Create SVG Path for plate
+        style = {'stroke': '#000000', 'fill': 'none', 'stroke-width': str(self.unittouu('1px'))}
+
+        #make bottom plate:
+        points=lc.make_plate(width,self.options.inner,depth,self.options.inner,
+                             thickness,self.options.wsplit,self.options.dsplit,
+                             'm',False,
+                             'm',False,
+                             'm',False,
+                             'm',False)
+        
+        path = lc.points_to_svgd(points)        
+        
+        box_attribs = {
+            'style': formatStyle(style),
+            'd': path}
+        box = inkex.etree.SubElement(
+            g, inkex.addNS('path', 'svg'), box_attribs)
+        
+        #make top plate:
+        points=lc.make_plate(width,self.options.inner,depth,self.options.inner,
+                             thickness,self.options.wsplit,self.options.dsplit,
+                             'm',False,
+                             'm',False,
+                             'm',False,
+                             'm',False)
+
+        points=lc.translate_points(points,width*1.1,depth*1.1)              
+        path = lc.points_to_svgd(points)  
+
+        
+        box_attribs = {
+            'style': formatStyle(style),
+            'd': path}
+        box = inkex.etree.SubElement(
+            g, inkex.addNS('path', 'svg'), box_attribs)
+        
+        #make back plate:
+        points=lc.make_plate(width,self.options.inner,height,self.options.inner,
+                             thickness,self.options.wsplit,self.options.hsplit,
+                             'f',False,
+                             'f',False,
+                             'f',False,
+                             'm',False)
+        points=lc.translate_points(points,0,-(depth*1.1))
+        path = lc.points_to_svgd(points)        
+        
+        box_attribs = {
+            'style': formatStyle(style),
+            'd': path}
+        box = inkex.etree.SubElement(
+            g, inkex.addNS('path', 'svg'), box_attribs)
+        
+        #make front plate:
+        points=lc.make_plate(width,self.options.inner,height,self.options.inner,
+                             thickness,self.options.wsplit,self.options.hsplit,
+                             'f',False,
+                             'f',False,
+                             'm',False,
+                             'f',False)
+        points=lc.translate_points(points,0,height*1.1)
+        path = lc.points_to_svgd(points)        
+        
+        box_attribs = {
+            'style': formatStyle(style),
+            'd': path}
+        box = inkex.etree.SubElement(
+            g, inkex.addNS('path', 'svg'), box_attribs)
+        
+        #make left plate:
+        points=lc.make_plate(depth,self.options.inner,height,self.options.inner,
+                             thickness,self.options.dsplit,self.options.hsplit,
+                             'm',False,
+                             'f',False,
+                             'f',False,
+                             'f',False)
+        points=lc.translate_points(points,-(depth*1.1),0)
+        path = lc.points_to_svgd(points)        
+        
+        box_attribs = {
+            'style': formatStyle(style),
+            'd': path}
+        box = inkex.etree.SubElement(
+            g, inkex.addNS('path', 'svg'), box_attribs)
+        
+        #make right plate:
+        points=lc.make_plate(depth,self.options.inner,height,self.options.inner,
+                             thickness,self.options.dsplit,self.options.hsplit,
+                             'm',False,
+                             'f',False,
+                             'f',False,
+                             'f',False)
+        points=lc.translate_points(points,width*1.1,0)
+        path = lc.points_to_svgd(points)        
+        
+        box_attribs = {
+            'style': formatStyle(style),
+            'd': path}
+        box = inkex.etree.SubElement(
+            g, inkex.addNS('path', 'svg'), box_attribs)
+        
+
+
+effect = FullBoxEffect()
+effect.affect()
+
+
+            
+        
