145baa14152bd584ff0a0cdba3baa2bf161f757a
David Renault <renault@enseirb-matmeca.fr>
2024-10-01 22:59:22 +0200

    Add types to the first third of the lc2 module

--- lc2.py
+++ lc2.py
@@ -1,102 +1,108 @@
 #!/usr/bin/python
+from __future__ import annotations # For self-reference of types in classes
+from typing import IO, Optional
 from enum import Enum, auto
-
 
 ################################################################
 class Point:
     """A class that represents a 2D-point. And also potentially a
     2D-vector."""
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self._x = x
         self._y = y
 
     @property
-    def x(self): return self._x
+    def x(self) -> float: return self._x
     @property
-    def y(self): return self._y
+    def y(self) -> float: return self._y
 
-    def add(self, other):
+    def add(self, other: Point) -> Point:
         return Point(self.x + other.x, self.y + other.y)
-    def sub(self, other):
+    def sub(self, other: Point) -> Point:
         return Point(self.x - other.x, self.y - other.y)
-    def mult(self, scalar):
+    def mult(self, scalar: float) -> Point:
         return Point(self.x*scalar, self.y*scalar)
-    def length(self):
-        return (self.x**2 + self.y**2)**0.5
-    def unit(self):
+    def length(self) -> float:
+        return float((self.x**2 + self.y**2)**0.5)
+    def unit(self) -> Point:
         len = self.length()
         return Point(self.x/len, self.y/len)
-    def perp(self):
+    def perp(self) -> Point:
         return Point(-self.y, self.x)
 
-    def min(self, other):
+    def min(self, other: Point) -> Point:
         return Point(min(self.x, other.x), min(self.y, other.y))
 
-    def max(self, other):
+    def max(self, other: Point) -> Point:
         return Point(max(self.x, other.x), max(self.y, other.y))
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         """Overrides the default implementation"""
         if isinstance(other, Point):
             return self.x == other.x and self.y == other.y
         return False
 
-    def __ne__(self, other):
+    def __ne__(self, other: object) -> bool:
         """Overrides the default implementation (unnecessary in Python 3)"""
         return not self.__eq__(other)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"({self.x},{self.y})"
 
 # Alias for the Point constructor
-def P(x,y):
+def P(x: float,y: float) -> Point:
     return Point(x, y)
 
 
 ################################################################
 class BoundingBox:
-    """ A class that represents a 2D bounding box """
-    def __init__(self, min, max):
+    """A class that represents a 2D bounding box
+
+    Nothing forces min and max to be ordered in the plane, they are
+    just the two extremities of the bounding box.
+
+    """
+    def __init__(self, min: Point, max: Point):
         self._min = min
         self._max = max
 
     @property
-    def min(self):  return self._min
+    def min(self) -> Point:  return self._min
     @property
-    def max(self):  return self._max
+    def max(self) -> Point:  return self._max
     @property
-    def xmin(self): return self._min.x
+    def xmin(self) -> float: return self._min.x
     @property
-    def xmax(self): return self._max.x
+    def xmax(self) -> float: return self._max.x
     @property
-    def ymin(self): return self._min.y
+    def ymin(self) -> float: return self._min.y
     @property
-    def ymax(self): return self._max.y
+    def ymax(self) -> float: return self._max.y
     @property
-    def lowerleft(self): return self._min
+    def lowerleft(self) -> Point: return self._min
     @property
-    def lowerright(self): return Point(self.xmax, self.ymin)
+    def lowerright(self) -> Point: return Point(self.xmax, self.ymin)
     @property
-    def upperright(self): return self._max
+    def upperright(self) -> Point: return self._max
     @property
-    def upperleft(self): return Point(self.xmin, self.ymax)
+    def upperleft(self) -> Point: return Point(self.xmin, self.ymax)
     @property
-    def width(self): return self._max.x - self._min.x
+    def width(self) -> float: return self._max.x - self._min.x
     @property
-    def height(self): return self._max.y - self._min.y
+    def height(self) -> float: return self._max.y - self._min.y
 
     # Side-effects methods
-    def union(self, other):
+    def union(self, other: BoundingBox) -> None:
         self._min = self._min.min(other.min)
         self._max = self._max.max(other.max)
 
-    def translate(self, a_point):
+    def translate(self, a_point: Point) -> None:
         self._min = self._min.add(a_point)
         self._max = self._max.add(a_point)
-    def translate_to_origin(self):
+    def translate_to_origin(self) -> None:
         self.translate(self._min)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"BB(({self.xmin},{self.ymin})->({self.xmax},{self.ymax}))"
 
 
@@ -109,28 +115,29 @@ class Edge(BoundingBox):
     of the final printing, it remains in the 2D-world.
 
     """
-    def __init__(self, min, max):
+    def __init__(self, min: Point, max: Point):
         super().__init__(min, max)
-        self._touch  = []    # Edges on the extremities
-        self._attch  = []    # Other edges touching this one
+        self._touch: list[Edge] = []    # Edges on the extremities
+        self._attch: list[Edge] = []    # Other edges touching this one
 
     @property
-    def origin(self):   return self._min
+    def origin(self) -> Point:   return self._min
     @property
-    def dest(self):     return self._max
+    def dest(self) -> Point:     return self._max
     @property
-    def length(self):   return ((self.dest.x-self.origin.x)**2. +
-                                (self.dest.y-self.origin.y)**2.)**0.5
+    def length(self) -> float:
+        return float(((self.dest.x-self.origin.x)**2. +
+                      (self.dest.y-self.origin.y)**2.)**0.5)
     @property
-    def attached_lengths(self):
-        def al(an_edge):
+    def attached_lengths(self) -> list[float]:
+        def al(an_edge: Edge) -> float:
             touching = an_edge.origin if self.contains(an_edge.origin) \
                 else an_edge.dest
             return touching.sub(self.origin).length()
         return list(map(al, self._attch))
 
     # Given a point p, test if p is colinear with the edge
-    def colinear(self, p, precision = 0.001):
+    def colinear(self, p: Point, precision: float = 0.001) -> bool:
         v1 = p.sub(self.origin)
         v2 = self.dest.sub(self.origin)
         return abs(v1.x*v2.y-v1.y*v2.x) < precision
@@ -138,146 +145,49 @@ class Edge(BoundingBox):
     # Given a point p that is colinear with the edge, returns
     # the distance relative to the origin (0 means the origin, 1 means
     # the dest, the result can be negative)
-    def distorig(self, p):
+    def distorig(self, p: Point) -> float:
         v1  = p.sub(self.origin)
         nv1 = (v1.x**2. + v1.y**2.)**0.5
         v2  = self.dest.sub(self.origin)
         nv2 = (v2.x**2. + v2.y**2.)**0.5
         if (v1.x*v2.x + v1.y*v2.y < 0):
-            return -nv1/nv2
+            return float(-nv1/nv2)
         else:
-            return nv1/nv2
+            return float(nv1/nv2)
 
     # Test if a point p belongs strictly to the edge
-    def contains(self, p):
+    def contains(self, p: Point) -> bool:
         if self.colinear(p):
             do = self.distorig(p)
             return (do > 0) and (do < 1)
         else:
             return False
 
-    def attach_extremity(self, edge):
+    def attach_extremity(self, edge: Edge) -> None:
         self._touch.append(edge)
-    def attach_middle(self, edge):
+    def attach_middle(self, edge: Edge) -> None:
         self._attch.append(edge)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Edge(({self.xmin},{self.ymin})->({self.xmax},{self.ymax}))"
 
 
 ################################################################
-class Hole(Edge):
-    """A class that represents an edge only for holes.
+class PlateBorderStyle(Enum):
+    """A class representing the possible types of edges in the
+    application
 
-    Typically, it represents a set of holes into which another edge
-    can possibly attach itself.
+    The protruding / caving in nomenclature is based on the start of
+    the edge.
 
     """
-    def __init__(self, _min, _max, _type):
-        super().__init__(_min, _max)
-        self._type = _type
 
-    @property
-    def type(self): return self._type
-
-    def __repr__(self):
-        return f"Hole(({self.xmin},{self.ymin})->({self.xmax},{self.ymax}))"
-
-
-################################################################
-class SvgPrinter:
-    """ A class that represents a svg printer object """
-    def __init__(self, edges, filename, margin = 10,
-                 color="black", stroke_width=0.01):
-        self._edges = edges
-        self._file = open(filename, "w")
-        self._margin = margin
-        self._color = color
-        self._stroke_width = stroke_width
-
-    def write(self, a_string):
-        self._file.write(a_string + "\n")
-
-    def set_color(self, a_color):
-        self._color = a_color
-    def set_stroke_width(self, a_length):
-        self._stroke_width = a_length
-
-    def print_header(self, bb):
-        self.write("<?xml version='1.0' encoding='UTF-8'?>\n")
-        viewBox = f"{bb.xmin} " + \
-            f"{bb.ymin} " + \
-            f"{bb.width} " + \
-            f"{bb.height}"
-        self.write(f"<svg width='{bb.width}cm' " +
-                   f"viewBox='{viewBox}' " +
-                   f"height='{bb.height}cm' " +
-                   f"xmlns='http://www.w3.org/2000/svg'>")
-
-    def print_rectangle(self, min, max, color=None, stroke_width=None):
-        col = self._color if color is None else color
-        sw  = self._stroke_width if stroke_width is None else stroke_width
-        min_x = min.x if min.x < max.x else max.x
-        min_y = min.y if min.y < max.y else max.y
-        width = abs(max.x - min.x)
-        height = abs(max.y - min.y)
-        # print("print rectangle", min, max, width, height)
-        self.write(f"  <rect width='{width}' height='{height}' " +
-                   f"x='{min_x}' y='{min_y}' " +
-                   f"style='fill:none;stroke-width:{sw};stroke:{col}' " +
-                   f"/>")
-
-    def print_segment(self, min, max, color=None, stroke_width=None):
-        col = self._color if color is None else color
-        sw  = self._stroke_width if stroke_width is None else stroke_width
-        self.write(f"<path d='M{min.x} {min.y} L{max.x} {max.y}' " +
-                   f"style='stroke-width:{sw};stroke:{col}'/>")
-
-    def start_group(self, id, label):
-        self.write(f"<g>")
-        # self.write(f"<g id='{id}' inkscape:label='{label} ' " + \
-        #            f"inkscape:groupmode='layer'>")
-
-    def end_group(self):
-        self.write(f"</g>")
-
-    def print_footer(self):
-        self.write("</svg>")
-
-    def dump_profile_to_svg(self):
-        self.print_header(self._edges.bb)
-        self.start_group(0, "Global rectangle")
-        self.print_rectangle(self._edges.bb.lowerleft,
-                             self._edges.bb.upperright)
-        self.end_group()
-        for an_edge in self._edges.edges:
-            self.print_segment(an_edge.origin, an_edge.dest)
-        self.close()
-
-    def dump_plates_to_svg(self):
-        plates = self._edges.plates
-        self.print_header(plates.bb)
-        self.start_group(0, "Global rectangle")
-        self.print_rectangle(plates.bb.lowerleft, plates.bb.upperright,
-                             color="red")
-        self.end_group()
-        for id_plate, a_plate in enumerate(plates):
-            a_plate.dump_to_svg(self, f"layer{id_plate}")
-        self.close()
-
-    def close(self):
-        self.print_footer()
-        self._file.close()
-
-
-################################################################
-class PlateBorderStyle(Enum):
     NONE = auto()                    # No border is drawn
     STRAIGHT = auto()                # Border is straight
     CRENELATED_PROTRUDING = auto()   # Border starts |̅|_|...
     CRENELATED_CAVING_IN = auto()    # Border starts _|̅|_|...
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         match self:
             case PlateBorderStyle.NONE:
                 return "none"
@@ -293,44 +203,156 @@ class PlateBorderStyle(Enum):
 
 ################################################################
 class PlateBorderType:
-    def __init__(self, style, start, end):
-        self._style = style
-        self._start_offset = start
-        self._end_offset = end
-        self._depth = None
+    def __init__(self, style: PlateBorderStyle, start: float, end: float):
+        self._style : PlateBorderStyle = style
+        self._start_offset : float     = start
+        self._end_offset : float       = end
+        self._depth : Optional[float]  = None
     @property
-    def style(self): return self._style
+    def style(self) -> PlateBorderStyle: return self._style
     @property
-    def start_offset(self): return self._start_offset
+    def start_offset(self) -> float: return self._start_offset
     @property
-    def end_offset(self): return self._end_offset
+    def end_offset(self) -> float: return self._end_offset
     @property
-    def depth(self): return self._depth
+    def depth(self) -> Optional[float]: return self._depth
 
     @staticmethod
-    def none():
+    def none() -> PlateBorderType:
         return PlateBorderType(PlateBorderStyle.NONE, 0, 0)
     @staticmethod
-    def straight(start=0, end=0):
+    def straight(start: float=0, end:float =0) -> PlateBorderType:
         return PlateBorderType(PlateBorderStyle.STRAIGHT, start, end)
     @staticmethod
-    def crenelated_protruding(start=0, end=0, depth=None):
+    def crenelated_protruding(start: float=0, end:float =0,
+                              depth:Optional[float] =None) -> PlateBorderType:
         p = PlateBorderType(PlateBorderStyle.CRENELATED_PROTRUDING, start, end)
         if depth:
             p._depth = depth
         return p
 
     @staticmethod
-    def crenelated_caving_in(start=0, end=0, depth=None):
+    def crenelated_caving_in(start:float =0, end:float =0,
+                             depth:Optional[float] =None) -> PlateBorderType:
         p = PlateBorderType(PlateBorderStyle.CRENELATED_CAVING_IN, start, end)
         if depth:
             p._depth = depth
         return p
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         d_str = f",depth={self._depth}" if self._depth else ""
         return f"Style({PlateBorderStyle.__repr__(self._style)}," + \
             f"start={self._start_offset},end={self._end_offset}{d_str})"
+
+
+################################################################
+class Hole(Edge):
+    """A class that represents a virtual edge that is lined with
+    holes.
+
+    Typically, it represents a set of holes into which another part of
+    the box can possibly attach itself.
+
+    """
+    def __init__(self, _min: Point, _max: Point, _type: PlateBorderType):
+        super().__init__(_min, _max)
+        self._type : PlateBorderType = _type
+
+    @property
+    def type(self) -> PlateBorderType: return self._type
+
+    def __repr__(self) -> str:
+        return f"Hole(({self.xmin},{self.ymin})->({self.xmax},{self.ymax}))"
+
+
+################################################################
+class SvgPrinter:
+    """ A class that represents a svg printer object """
+    def __init__(self, edges:Edges, filename:str,
+                 margin:float = 10, color:str = "black",
+                 stroke_width:float = 0.01):
+        self._edges : Edges        = edges
+        self._file : IO[str]       = open(filename, "w")
+        self._margin : float       = margin
+        self._color : str          = color
+        self._stroke_width : float = stroke_width
+
+    def write(self, a_string: str) -> None:
+        self._file.write(a_string + "\n")
+
+    def set_color(self, a_color: str) -> None:
+        self._color = a_color
+    def set_stroke_width(self, a_length: float) -> None:
+        self._stroke_width = a_length
+
+    def print_header(self, bb: BoundingBox) -> None:
+        self.write("<?xml version='1.0' encoding='UTF-8'?>\n")
+        viewBox = f"{bb.xmin} " + \
+            f"{bb.ymin} " + \
+            f"{bb.width} " + \
+            f"{bb.height}"
+        self.write(f"<svg width='{bb.width}cm' " +
+                   f"viewBox='{viewBox}' " +
+                   f"height='{bb.height}cm' " +
+                   f"xmlns='http://www.w3.org/2000/svg'>")
+
+    def print_rectangle(self, min: Point, max: Point,
+                        color: Optional[str] = None,
+                        stroke_width: Optional[float] = None) -> None:
+        col = self._color if color is None else color
+        sw  = self._stroke_width if stroke_width is None else stroke_width
+        min_x = min.x if min.x < max.x else max.x
+        min_y = min.y if min.y < max.y else max.y
+        width = abs(max.x - min.x)
+        height = abs(max.y - min.y)
+        self.write(f"  <rect width='{width}' height='{height}' " +
+                   f"x='{min_x}' y='{min_y}' " +
+                   f"style='fill:none;stroke-width:{sw};stroke:{col}' " +
+                   f"/>")
+
+    def print_segment(self, min: Point, max: Point,
+                      color: Optional[str] = None,
+                      stroke_width: Optional[float] = None) -> None:
+        col = self._color if color is None else color
+        sw  = self._stroke_width if stroke_width is None else stroke_width
+        self.write(f"<path d='M{min.x} {min.y} L{max.x} {max.y}' " +
+                   f"style='stroke-width:{sw};stroke:{col}'/>")
+
+    def start_group(self, id: str, label: str) -> None:
+        self.write(f"<g>")
+        # self.write(f"<g id='{id}' inkscape:label='{label} ' " + \
+        #            f"inkscape:groupmode='layer'>")
+
+    def end_group(self) -> None:
+        self.write(f"</g>")
+
+    def print_footer(self) -> None:
+        self.write("</svg>")
+
+    def dump_profile_to_svg(self) -> None:
+        self.print_header(self._edges.bb)
+        self.start_group("0", "Global rectangle")
+        self.print_rectangle(self._edges.bb.lowerleft,
+                             self._edges.bb.upperright)
+        self.end_group()
+        for an_edge in self._edges.edges:
+            self.print_segment(an_edge.origin, an_edge.dest)
+        self.close()
+
+    def dump_plates_to_svg(self) -> None:
+        plates = self._edges.plates
+        self.print_header(plates.bb)
+        self.start_group("0", "Global rectangle")
+        self.print_rectangle(plates.bb.lowerleft, plates.bb.upperright,
+                             color="red")
+        self.end_group()
+        for id_plate, a_plate in enumerate(plates):
+            a_plate.dump_to_svg(self, f"layer{id_plate}")
+        self.close()
+
+    def close(self) -> None:
+        self.print_footer()
+        self._file.close()
 
 
 ################################################################
@@ -800,5 +822,6 @@ if __name__ == '__main__':
 
 ################################################################
 # Test usage :
+# clear && mypy ./lc2.py && ./lc2.py
 # clear && ./lc2.py && cat file.svg && inkview file.svg &> /dev/null
 # clear && ./lc2.py && cat profile.svg && inkview -s 0.5 plates.svg &> /dev/null
